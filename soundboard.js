8529a27e07e7f7487d615eec690a42bc73390cbc
Oluwanifemi Olotu <olotuolu@msu.edu>
2022-06-15 17:52:57 -0500

    java fix

--- soundboard.js
+++ soundboard.js
@@ -1,6 +1,5 @@
 var bass1= document.getElementById("botton1")
 var bass1_sound= document.getElementById("bass1-sound")
-bass1.addEventListener('click',function(){
-    console.log('it was clicked')
+bass1.addEventListener(onclick=function(){
     bass1_sound.play()
 })
