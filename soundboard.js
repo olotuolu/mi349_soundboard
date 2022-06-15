38b3598a18a013d6faf659838dee0126df895eee
Oluwanifemi Olotu <olotuolu@msu.edu>
2022-06-15 18:14:12 -0500

    button rework

--- soundboard.js
+++ soundboard.js
@@ -1,5 +1,2 @@
-var bass1= document.getElementById("botton1")
-var bass1_sound= document.getElementById("bass1-sound")
-bass1.addEventListener(onclick=function(){
-    bass1_sound.play()
-})
+var bass1_sound= document.getElementById("bass1-sound");
+function bass1()
