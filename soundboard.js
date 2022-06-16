03aa9d8f74de62104b55257121eefaaddd01dfab
Oluwanifemi Olotu <olotuolu@msu.edu>
2022-06-15 19:48:37 -0500

    This better have worked

--- soundboard.js
+++ soundboard.js
@@ -4,3 +4,17 @@ wow_button.addEventListener('click',function(){
     console.log('it was clicked')
     wow_sound.play()
 })
+
+var mario_jump_button= document.getElementById("mario-jump")
+var mario_jump_sound= document.getElementById("mario-jump-sound")
+mario_jump_button.addEventListener('click',function(){
+    console.log('it was clicked')
+    mario_jump_sound.play()
+})
+
+var john_cena_button= document.getElementById("john-cena")
+var john_cena_sound= document.getElementById("john-cena-sound")
+john_cena_button.addEventListener('click',function(){
+    console.log('it was clicked')
+    john_cena_sound.play()
+})
