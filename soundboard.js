96376f2632a57cb4e54308f8c2973fff4a8c8838
Oluwanifemi Olotu <olotuolu@msu.edu>
2022-06-15 19:37:57 -0500

    this is not fun anymore

--- soundboard.js
+++ soundboard.js
@@ -2,5 +2,5 @@ var wow_button= document.getElementById("wow")
 var wow_sound= document.getElementById("wow-sound")
 wow_button.addEventListener('click',function(){
     console.log('it was clicked')
-    wow_sound.onplay()
+    wow_sound.play()
 })
